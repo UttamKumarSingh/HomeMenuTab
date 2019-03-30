47cf8f96f8d0b09ccf191c1aa96b7da905b63cf8
Uttam Kumar Singh <gksinghatsugi@gmail.com>
2019-03-30 12:02:39 +0900

    This is the first version where the MVVM pattern is not followed for data update

---
+++ app/src/main/java/com/example/uks/viewpagertestapp/ViewPagerFragmentViewModel.java
@@ -0,0 +1,11 @@
+package com.example.uks.viewpagertestapp;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+public class ViewPagerFragmentViewModel extends AndroidViewModel {
+    public ViewPagerFragmentViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
